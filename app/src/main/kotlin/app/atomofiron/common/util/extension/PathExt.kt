cd7e88568a67bc73117a4a7c538c8d5b57df9ea6
atomofiron <atomofiron@gmail.com>
2025-04-23 08:49:10 +0200

    simplify corner path math

--- app/src/main/kotlin/app/atomofiron/common/util/extension/PathExt.kt
+++ app/src/main/kotlin/app/atomofiron/common/util/extension/PathExt.kt
@@ -7,29 +7,15 @@ import kotlin.math.min
 import kotlin.math.sign
 
 fun Path.corner(x: Float, y: Float, left: Boolean, top: Boolean, clockWise: Boolean, radius: Float, offsetX: Float = 0f, offsetY: Float = 0f) {
-    var count = if (left) 0 else 1
-    count += if (top) 0 else 1
-    count += if (clockWise) 1 else 0
-    val x0 = x + when (count) {
-        0, 2 -> radius * dX(top, clockWise)
-        else -> 0f
-    }
-    val y0 = y + when (count) {
-        1, 3 -> radius * dY(left, clockWise)
-        else -> 0f
-    }
-    val x1 = x + when (count) {
-        1, 3 -> radius * dX(!top, clockWise)
-        else -> 0f
-    }
-    val y1 = y + when (count) {
-        0, 2 -> radius * dY(!left, clockWise)
-        else -> 0f
-    }
+    val flag = (left == top) == clockWise
+    val x0 = x + if (flag) 0f else radius * dX(top, clockWise)
+    val y0 = y + if (!flag) 0f else radius * dY(left, clockWise)
+    val x1 = x + if (!flag) 0f else radius * dX(!top, clockWise)
+    val y1 = y + if (flag) 0f else radius * dY(!left, clockWise)
     corner(x0, y0, x1, y1, clockWise, offsetX, offsetY)
 }
 
-fun Path.corner(x0: Float, y0: Float, x1: Float, y1: Float, clockWise: Boolean, offsetX: Float = 0f, offsetY: Float = 0f) {
+private fun Path.corner(x0: Float, y0: Float, x1: Float, y1: Float, clockWise: Boolean, offsetX: Float = 0f, offsetY: Float = 0f) {
     var dx = x1 - x0
     var dy = y1 - y0
     if (abs(dx) != abs(dy)) {
