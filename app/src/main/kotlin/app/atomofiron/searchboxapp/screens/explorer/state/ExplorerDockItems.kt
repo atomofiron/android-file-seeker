253285a4387f22023d26f3b39d4b944afe724973
atomofiron <atomofiron@gmail.com>
2025-05-07 03:57:53 +0200

    replace explorerDockItems() with ExplorerDockState

--- app/src/main/kotlin/app/atomofiron/searchboxapp/screens/explorer/state/ExplorerDockItems.kt
+++ app/src/main/kotlin/app/atomofiron/searchboxapp/screens/explorer/state/ExplorerDockItems.kt
@@ -1,6 +1,5 @@
 package app.atomofiron.searchboxapp.screens.explorer.state
 
-import android.graphics.drawable.Drawable
 import app.atomofiron.fileseeker.R
 import app.atomofiron.searchboxapp.custom.view.dock.item.DockItem
 
@@ -8,15 +7,22 @@ enum class ExplorerDock(override val value: Long) : DockItem.Id {
     Search(0), Settings(1)
 }
 
-fun explorerDockItems(settings: Drawable) = listOf(
-    DockItem(
-        ExplorerDock.Search,
-        DockItem.Icon(R.drawable.ic_search),
-        DockItem.Label(R.string.search),
-    ),
-    DockItem(
-        ExplorerDock.Settings,
-        DockItem.Icon(settings),
-        DockItem.Label(R.string.settings),
-    ),
-)
+data class ExplorerDockState(
+    val search: DockItem,
+    val settings: DockItem,
+) : List<DockItem> by listOf(search, settings) {
+    companion object {
+        val Default = ExplorerDockState(
+            search = DockItem(
+                ExplorerDock.Search,
+                DockItem.Icon(R.drawable.ic_search),
+                DockItem.Label(R.string.search),
+            ),
+            settings = DockItem(
+                ExplorerDock.Settings,
+                DockItem.Icon(R.drawable.ic_settings),
+                DockItem.Label(R.string.settings),
+            ),
+        )
+    }
+}
