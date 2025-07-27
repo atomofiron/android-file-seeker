757d7eb771765ff94bb5f1d88f8034d30eb011f2
atomofiron <atomofiron@gmail.com>
2025-07-27 06:34:26 +0200

    replace " with ' in shell command templates

--- app/src/main/kotlin/app/atomofiron/searchboxapp/utils/Shell.kt
+++ app/src/main/kotlin/app/atomofiron/searchboxapp/utils/Shell.kt
@@ -15,53 +15,53 @@ object Shell {
     private const val TOYBOX = "{toybox}"
     var toyboxPath = ToyboxVariant.System.path
 
-    const val TOUCH = "{toybox} touch \"%s\""
-    const val COPY = "{toybox} cp -r \"%s\" \"%s\""
+    const val TOUCH = "{toybox} touch '%s'"
+    const val COPY = "{toybox} cp -r '%s' '%s'"
     const val VERSION = "{toybox} --version"
-    const val MKDIR = "{toybox} mkdir \"%s\""
-    const val RM_RF = "{toybox} rm -rf \"%s\""
-    const val MV = "{toybox} mv \"%s\" \"%s\""
-    const val LS_LAL = "{toybox} ls -lAL \"%s\""
+    const val MKDIR = "{toybox} mkdir '%s'"
+    const val RM_RF = "{toybox} rm -rf '%s'"
+    const val MV = "{toybox} mv '%s' '%s'"
+    const val LS_LAL = "{toybox} ls -lAL '%s'"
     // can't use 'file -b' because of Android 10 contains toybox-0.8.0
-    const val LS_LALD_FILE = "{toybox} ls -lALd \"%s\" && {toybox} file \"%s\""
+    const val LS_LALD_FILE = "{toybox} ls -lALd '%s' && {toybox} file '%s'"
     // 'du -hd 1' is useless because of any error don't allow get info about items below dir with error (Permission denied)
-    const val DU_HD0 = "{toybox} du -hd 0 \"%s\""
+    const val DU_HD0 = "{toybox} du -hd 0 '%s'"
     // DOT_SLASH is needed because of this shit:
     // $ cd /some/dir && file *
     // file: Unknown option '-file-name-starting-with-dash.png' (see "file --help")
-    const val CD_FILE_CHILDREN = "cd \"%s\" && {toybox} file $DOT_SLASH* && {toybox} file $DOT_SLASH.*"
+    const val CD_FILE_CHILDREN = "cd '%s' && {toybox} file $DOT_SLASH* && {toybox} file $DOT_SLASH.*"
 
     // grep: No 'E' with 'F'
-    const val FIND_GREP_HCS = "{toybox} find \"%s\" -type f -maxdepth %d | xargs {toybox} file | {toybox} grep -E 'ASCII text$' | awk -F: '{print \$1}' | xargs {toybox} grep -Hcs -e \"%s\""
-    const val FIND_GREP_HCS_I = "{toybox} find \"%s\" -type f -maxdepth %d | xargs {toybox} file | {toybox} grep -E 'ASCII text$' | awk -F: '{print \$1}' | xargs {toybox} grep -Hcs -ie \"%s\""
-    const val FIND_GREP_HCS_E = "{toybox} find \"%s\" -type f -maxdepth %d | xargs {toybox} file | {toybox} grep -E 'ASCII text$' | awk -F: '{print \$1}' | xargs {toybox} grep -Hcs -E \"%s\""
-    const val FIND_GREP_HCS_IE = "{toybox} find \"%s\" -type f -maxdepth %d | xargs {toybox} file | {toybox} grep -E 'ASCII text$' | awk -F: '{print \$1}' | xargs {toybox} grep -Hcs -iE \"%s\""
+    const val FIND_GREP_HCS = "{toybox} find '%s' -type f -maxdepth %d | xargs {toybox} file | {toybox} grep -E 'ASCII text$' | awk -F: '{print \$1}' | xargs {toybox} grep -Hcs -e '%s'"
+    const val FIND_GREP_HCS_I = "{toybox} find '%s' -type f -maxdepth %d | xargs {toybox} file | {toybox} grep -E 'ASCII text$' | awk -F: '{print \$1}' | xargs {toybox} grep -Hcs -ie '%s'"
+    const val FIND_GREP_HCS_E = "{toybox} find '%s' -type f -maxdepth %d | xargs {toybox} file | {toybox} grep -E 'ASCII text$' | awk -F: '{print \$1}' | xargs {toybox} grep -Hcs -E '%s'"
+    const val FIND_GREP_HCS_IE = "{toybox} find '%s' -type f -maxdepth %d | xargs {toybox} file | {toybox} grep -E 'ASCII text$' | awk -F: '{print \$1}' | xargs {toybox} grep -Hcs -iE '%s'"
     // /storage/emulated/0/fadb/sba.txt:15
 
     // -H is necessary
-    const val GREP_HCS = "{toybox} grep -Hcs -e \"%s\" \"%s\""
-    const val GREP_HCS_I = "{toybox} grep -Hcs -ie \"%s\" \"%s\""
-    const val GREP_HCS_E = "{toybox} grep -Hcs -E \"%s\" \"%s\""
-    const val GREP_HCS_IE = "{toybox} grep -Hcs -iE \"%s\" \"%s\""
+    const val GREP_HCS = "{toybox} grep -Hcs -e '%s' '%s'"
+    const val GREP_HCS_I = "{toybox} grep -Hcs -ie '%s' '%s'"
+    const val GREP_HCS_E = "{toybox} grep -Hcs -E '%s' '%s'"
+    const val GREP_HCS_IE = "{toybox} grep -Hcs -iE '%s' '%s'"
     // /storage/emulated/0/fadb/sba.txt:15
 
     // \( %s \) -exec {toybox} grep -H -c -s -E "work" {} \;"
-    const val FIND_EXEC_GREP = "{toybox} find \"%s\" -type f -maxdepth %d \\( %s \\) -exec {toybox} grep -Hcs -E \"%s\" {} \\;"
-    const val FIND_EXEC_GREP_I = "{toybox} find \"%s\" -type f -maxdepth %d \\( %s \\) -exec {toybox} grep -Hcs -iE \"%s\" {} \\;"
-    const val FIND_EXEC_GREP_E = "{toybox} find \"%s\" -type f -maxdepth %d \\( %s \\) -exec {toybox} grep -Hcs -E \"%s\" {} \\;"
-    const val FIND_EXEC_GREP_IE = "{toybox} find \"%s\" -type f -maxdepth %d \\( %s \\) -exec {toybox} grep -Hcs -iE \"%s\" {} \\;"
+    const val FIND_EXEC_GREP = "{toybox} find '%s' -type f -maxdepth %d \\( %s \\) -exec {toybox} grep -Hcs -E '%s' {} \\;"
+    const val FIND_EXEC_GREP_I = "{toybox} find '%s' -type f -maxdepth %d \\( %s \\) -exec {toybox} grep -Hcs -iE '%s' {} \\;"
+    const val FIND_EXEC_GREP_E = "{toybox} find '%s' -type f -maxdepth %d \\( %s \\) -exec {toybox} grep -Hcs -E '%s' {} \\;"
+    const val FIND_EXEC_GREP_IE = "{toybox} find '%s' -type f -maxdepth %d \\( %s \\) -exec {toybox} grep -Hcs -iE '%s' {} \\;"
 
-    const val FIND_FD = "{toybox} find \"%s\" -maxdepth %d \\( -type f -o -type d \\)"
-    const val FIND_F = "{toybox} find \"%s\" -maxdepth %d -type f"
+    const val FIND_FD = "{toybox} find '%s' -maxdepth %d \\( -type f -o -type d \\)"
+    const val FIND_F = "{toybox} find '%s' -maxdepth %d -type f"
 
-    const val HEAD_TAIL = "{toybox} head \"%s\" -n %d | {toybox} tail -n %d"
-    const val LS_LOG = "{toybox} ls -log \"%s\""
+    const val HEAD_TAIL = "{toybox} head '%s' -n %d | {toybox} tail -n %d"
+    const val LS_LOG = "{toybox} ls -log '%s'"
     const val CP_F = "{toybox} cp -f %s %s"
 
-    const val GREP_BONS = "{toybox} grep -bons -e \"%s\" \"%s\""
-    const val GREP_BONS_I = "{toybox} grep -bons -ie \"%s\" \"%s\""
-    const val GREP_BONS_E = "{toybox} grep -bons -E \"%s\" \"%s\""
-    const val GREP_BONS_IE = "{toybox} grep -bons -iE \"%s\" \"%s\""
+    const val GREP_BONS = "{toybox} grep -bons -e '%s' '%s'"
+    const val GREP_BONS_I = "{toybox} grep -bons -ie '%s' '%s'"
+    const val GREP_BONS_E = "{toybox} grep -bons -E '%s' '%s'"
+    const val GREP_BONS_IE = "{toybox} grep -bons -iE '%s' '%s'"
     // 241:6916:work
 
     // %s grep -c -s -F -i -e "%s" "%s"
