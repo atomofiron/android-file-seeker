6838bc7580eca930ac05f52859a8dbea6e3a3a04
atomofiron <atomofiron@gmail.com>
2025-09-20 19:38:50 +0200

    make Rslt class

--- app/src/main/kotlin/app/atomofiron/searchboxapp/utils/Rslt.kt
+++ app/src/main/kotlin/app/atomofiron/searchboxapp/utils/Rslt.kt
@@ -2,21 +2,22 @@ package app.atomofiron.searchboxapp.utils
 
 import app.atomofiron.common.util.forHumans
 
-sealed interface Rslt<T> {
-    interface Ok<T> : Rslt<T> {
-        companion object : Ok<Unit> {
-            private data class Ok<T>(override val data: T) : Rslt.Ok<T>
+sealed class Rslt<T>(val isOk: Boolean) {
+
+    abstract class Ok<T> : Rslt<T>(isOk = true) {
+        companion object : Rslt.Ok<Unit>() {
+            private data class Ok<T>(override val data: T) : Rslt.Ok<T>()
             override val data = Unit
             operator fun <T> invoke(data: T): Rslt.Ok<T> = Ok(data)
         }
-        val data: T
+        abstract val data: T
     }
-    interface Err<T> : Rslt<T> {
+    abstract class Err<T> : Rslt<T>(isOk = false) {
         companion object {
-            private data class Err<T>(override val message: String) : Rslt.Err<T>
+            private data class Err<T>(override val message: String) : Rslt.Err<T>()
             operator fun <T> invoke(message: String): Rslt.Err<T> = Err(message)
         }
-        val message: String get() = ""
+        open val message: String get() = ""
     }
 }
 
