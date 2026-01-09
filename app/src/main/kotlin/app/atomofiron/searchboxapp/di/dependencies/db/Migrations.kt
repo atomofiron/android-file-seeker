89f1d6bb8dc929c1f5d149ad1121bd9c5c715476
atomofiron <atomofiron@gmail.com>
2026-01-09 04:14:33 +0100

    fix Migrations.MIGRATION_2_3

--- app/src/main/kotlin/app/atomofiron/searchboxapp/di/dependencies/db/Migrations.kt
+++ app/src/main/kotlin/app/atomofiron/searchboxapp/di/dependencies/db/Migrations.kt
@@ -8,11 +8,11 @@ object Migrations {
     val MIGRATION_2_3 get() = object : Migration(2, 3) {
         override fun migrate(db: SupportSQLiteDatabase) {
             db.execSQL("""
-            CREATE TABLE `${ExplorerDao.SORTING}` (
-                `tabIndex` INTEGER NOT NULL,
-                `rootId` TEXT NOT NULL,
-                `sorting` TEXT,
-                PRIMARY KEY(`tabIndex`, `rootId`)
+            CREATE TABLE ${ExplorerDao.SORTING} (
+                tabIndex INTEGER NOT NULL,
+                rootId INTEGER NOT NULL,
+                sorting TEXT,
+                PRIMARY KEY(tabIndex, rootId)
             )
             """.trimIndent())
         }
