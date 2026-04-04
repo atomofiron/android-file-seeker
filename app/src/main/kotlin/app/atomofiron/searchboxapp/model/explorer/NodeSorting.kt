4fa3c8f5cc4035339448e8db5df22152ffe262e1
atomofiron <atomofiron@gmail.com>
2026-04-04 17:39:37 +0200

    fix NodeSorting serialization

--- app/src/main/kotlin/app/atomofiron/searchboxapp/model/explorer/NodeSorting.kt
+++ app/src/main/kotlin/app/atomofiron/searchboxapp/model/explorer/NodeSorting.kt
@@ -1,42 +1,92 @@
 package app.atomofiron.searchboxapp.model.explorer
 
 import app.atomofiron.searchboxapp.custom.view.dock.item.DockItem
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlin.LazyThreadSafetyMode.NONE
 
-@Serializable
+@Serializable(with = NodeSortingSerializer::class)
 sealed class NodeSorting(val reversed: Boolean) : DockItem.Id.Auto() {
+
+    abstract val name: String
+
     sealed class Name(reversed: Boolean) : NodeSorting(reversed) {
+
         data object Reversed : Name(reversed = true) {
-            override fun toString() = "Name.Reversed"
+
+            override val name = "${Name.name}.$ReversedName"
+
+            override fun toString() = name
         }
+
         companion object : Name(reversed = false) {
-            operator fun invoke(reversed: Boolean): NodeSorting = if (reversed) Reversed else this
-            override fun toString() = "Name"
+
+            override val name = "Name"
+
+            override fun toString() = name
         }
     }
+
     sealed class Date(reversed: Boolean) : NodeSorting(reversed) {
+
         data object Reversed : Date(reversed = true) {
-            override fun toString() = "Date.Reversed"
+
+            override val name = "${Date.name}.$ReversedName"
+
+            override fun toString() = name
         }
+
         companion object : Date(reversed = false) {
-            operator fun invoke(reversed: Boolean): NodeSorting = if (reversed) Reversed else this
-            override fun toString() = "Date"
+
+            override val name = "Date"
+
+            override fun toString() = name
         }
     }
+
     sealed class Size(reversed: Boolean) : NodeSorting(reversed) {
+
         data object Reversed : Size(reversed = true) {
-            override fun toString() = "Size.Reversed"
+
+            override val name = "${Size.name}.$ReversedName"
+
+            override fun toString() = name
         }
+
         companion object : Size(reversed = false) {
-            operator fun invoke(reversed: Boolean): NodeSorting = if (reversed) Reversed else this
-            override fun toString() = "Size"
+
+            override val name = "Size"
+
+            override fun toString() = name
         }
     }
+
     companion object {
+
+        const val BaseName = "NodeSorting"
+        private const val ReversedName = "Reversed"
 
         val entries by lazy(NONE) { listOf(Name, Name.Reversed, Date, Date.Reversed, Size, Size.Reversed) }
 
         operator fun invoke(id: DockItem.Id): NodeSorting? = entries.find { it == id }
+    }
+}
+
+private object NodeSortingSerializer : KSerializer<NodeSorting> {
+
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(NodeSorting.BaseName, PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: NodeSorting) = encoder.encodeString(value.name)
+
+    override fun deserialize(decoder: Decoder): NodeSorting {
+        val name = decoder.decodeString()
+        return NodeSorting.entries.find { it.name == name }
+            ?: throw SerializationException("Unknown ${NodeSorting.BaseName} name: $name")
     }
 }
