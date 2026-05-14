a39f64c8aeb67cebe81e00c11bade87d9cce6a7b
atomofiron <atomofiron@gmail.com>
2026-05-14 08:24:07 +0400

    up the app version to 1.5.4 (33)

--- build-logic/convention/src/main/kotlin/app/fileseeker/convention/AppConfig.kt
+++ build-logic/convention/src/main/kotlin/app/fileseeker/convention/AppConfig.kt
@@ -18,6 +18,6 @@ object AppConfig {
     const val compileSdk = 36
     const val buildToolsVersion = "36.0.0"
 
-    const val versionCode = 32
-    const val versionName = "1.5.3"
+    const val versionCode = 33
+    const val versionName = "1.5.4"
 }
